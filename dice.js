521b75b47fe272d3a886b9805f2b2379508017f8
ZejunZhou <zzhou443>
2023-01-13 21:22:35 +0800

    adding the button with playing functionality

--- dice.js
+++ dice.js
@@ -20,22 +20,26 @@ function diceComparison(dice1, dice2){
     return winner;
 }
 
-var dice1 = generateRandomDice();
-var dice2 = generateRandomDice();
+function generateResult(){
+    var dice1 = generateRandomDice();
+    var dice2 = generateRandomDice();
 
-var dice1Path = "images/dice" + dice1 + ".png";
-var dice2Path = "images/dice" + dice2 + ".png";
+    var dice1Path = "images/dice" + dice1 + ".png";
+    var dice2Path = "images/dice" + dice2 + ".png";
 
-var result = diceComparison(dice1, dice2);
+    var result = diceComparison(dice1, dice2);
 
-document.querySelector(".img1").setAttribute("src", dice1Path);
-document.querySelector(".img2").setAttribute("src", dice2Path);
+    document.querySelector(".img1").setAttribute("src", dice1Path);
+    document.querySelector(".img2").setAttribute("src", dice2Path);
 
-if (result == "dice1"){
-    document.querySelector("h1").textContent = "Player1 win!";
-}else if (result == "dice2"){
-    document.querySelector("h1").textContent = "Player2 win!";
-}else{
-    document.querySelector("h1").textContent = "Draw";
+    if (result == "dice1"){
+        document.querySelector("h1").textContent = "Player1 win!";
+    }else if (result == "dice2"){
+        document.querySelector("h1").textContent = "Player2 win!";
+    }else{
+        document.querySelector("h1").textContent = "Draw";
+}
+
+
 }
 
